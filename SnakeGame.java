af7b11f49747375cc7563516c471299165f46b34
tosinidowu <86814979+tosinidowu@users.noreply.github.com>
2021-10-17 15:15:03 -0400

    first commit

---
+++ SnakeGame.java
@@ -0,0 +1,16 @@
+import java.awt.Color;
+  
+import javax.swing.JFrame;
+
+
+
+public class SnakeGame
+{
+  public static void main(String[] args)
+  {
+    
+    GameFrame frame = new GameFrame();
+  
+  }
+
+}
